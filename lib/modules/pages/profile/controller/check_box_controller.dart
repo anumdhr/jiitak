86b68d710b589b785ba2cd82bbdc3da308c727f8
Anu Manandhar <anumdr3@gmail.com>
2024-06-17 08:41:26 +0545

    updates

--- lib/modules/pages/profile/controller/check_box_controller.dart
+++ lib/modules/pages/profile/controller/check_box_controller.dart
@@ -1,10 +1,8 @@
 import 'package:get/get.dart';
 
 class CheckboxController extends GetxController {
-  // Create a list to hold the state of each checkbox
   var checkboxes = List<bool>.filled(8, false).obs;
 
-  // Method to toggle the state of a checkbox
   void toggleCheckbox(int index) {
     checkboxes[index] = !checkboxes[index];
   }
